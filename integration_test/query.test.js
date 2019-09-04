66071fcb0853be8f091b1b86ac5f967377c2fbc6
Gabriel Reid <gabriel@waylay.io>
2019-09-04 09:31:12 +0200

    chore: use https in integration test
    Also update connection_info test to be less sensitive to
    internal hostnames.

--- integration_test/query.test.js
+++ integration_test/query.test.js
@@ -8,21 +8,21 @@ beforeEach(() => {
 
 test('test connection info query', () => {
   return expect(
-    connect('http://sql-connector-staging.waylay.io/',
+    connect('https://sql-connector-staging.waylay.io/',
       process.env.STAGING_CLIENT_ID, process.env.STAGING_CLIENT_SECRET)
       .then(conn => {
-        return conn.query('select engine_host eng_host_name from waylay.connection_info').then(
+        return conn.query('select count(*) cnt from waylay.connection_info').then(
           resultSet => {
             conn.close()
             return resultSet
           }
         )
-      })).resolves.toMatchObject(new ResultSet(['ENG_HOST_NAME'], [['staging.waylay.io']]))
+      })).resolves.toMatchObject(new ResultSet(['CNT'], [[1]]))
 })
 
 test('test resource count query', () => {
   return expect(
-    connect('http://sql-connector-staging.waylay.io/',
+    connect('https://sql-connector-staging.waylay.io/',
       process.env.STAGING_CLIENT_ID, process.env.STAGING_CLIENT_SECRET)
       .then(conn => {
         return conn.query('select count(*) from waylay.resource where id = \'abc\'').then(
